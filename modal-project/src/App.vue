a7964810b6d7ef16912456fc0d26dbf96cd7f927
Gerald Landl <42845008+glandl@users.noreply.github.com>
2022-03-10 09:54:08 +0100

    Initialize lesson-09

---
+++ modal-project/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Wellcome ...</p>
+  
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1> {{ header }} </h1>
+      <p>{{ text }}</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModal2">
+    <Modal theme="sale" @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data(){
+    return {
+      title: "My First Vue App :)",
+      header: "Sign up for a givaway!",
+      text: "Grab your ninja swag for half price!",
+      showModal: false,
+      showModal2: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModal2 = !this.showModal2
+    }
+  }
+}
+</script>
+
+<style>
+#app, .modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
