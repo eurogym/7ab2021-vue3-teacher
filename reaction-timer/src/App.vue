fb157b8448dc9d5f650c0712733701d5af288e6c
Gerald Landl <glandl@eurogym.info>
2022-03-17 12:07:30 +0100

    2022-03-01

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -2,6 +2,7 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <p>Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
@@ -18,12 +19,17 @@ export default {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
     }
   },
   methods:{
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
     }
   }
 }
