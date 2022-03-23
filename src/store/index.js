2b203716722082e70fefaa6d4f60ca2edc353dfc
Gerald Landl <glandl@duality.at>
2022-03-23 19:01:45 +0100

    Lesson3

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    points: 0
   },
   mutations: {
+    updatePoints(state, payload) {
+      state.points = state.points + payload
+    }
   },
   actions: {
   },
